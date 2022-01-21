b747945d704cd12027b40b8a930b02616f860432
baroni51 <98057225+baroni51@users.noreply.github.com>
2022-01-21 13:23:35 -0700

    Added up and down

--- Proj1/Assets/Player.cs
+++ Proj1/Assets/Player.cs
@@ -15,19 +15,27 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.forward * 0.25f; 
+            transform.position += Vector3.forward * 0.01f; 
         }
         if (Input.GetKey(KeyCode.W))
         { 
-            transform.position += Vector3.back * 0.25f;
+            transform.position += Vector3.back * 0.01f;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.right * 0.25f;
+            transform.position += Vector3.right * 0.01f;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.left * 0.25f;
+            transform.position += Vector3.left * 0.01f;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += Vector3.up * 0.01f;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.position += Vector3.down * 0.01f;
         }
     }
 }
